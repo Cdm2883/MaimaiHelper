b97ccaae73ebf96552f79db4ddd239b22281f9a9
Cdm2883 <Cdm2883@hotmail.com>
2025-01-24 23:07:35 +0800

    refactor: setup minimalist android project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -29,12 +27,5 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    implementation(libs.androidx.appcompat)
 }
